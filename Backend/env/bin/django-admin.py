669e860c3fb1dda9114ccd7f6cf51d799d0cad4d
palwesh_wgt <palwesh@webgentechnologies.com>
2020-08-31 11:14:56 +0530

    frontend added

---
+++ Backend/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/palwesh_here/Fracal_analytics_Assignment_fullstack/Backend/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
